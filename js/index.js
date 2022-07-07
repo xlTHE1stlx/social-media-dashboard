227cb8b3c9c232dd7c92336a44df29ae9b4f8b1b
Juan Olaya <jdos46juan@gmail.com>
2022-07-07 18:41:02 -0500

    change the elements that has hover actions, create the color for the elements with hover actions and base to create the button toggle theme with javascript

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 window.onload = () => {
-	if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	if (
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches &&
+		localStorage.getItem("theme") === "dark"
+	) {
 		// dark mode
-		localStorage.setItem("dark", "1");
-		console.log(localStorage.getItem("dark"));
+		localStorage.setItem("theme", "dark");
+		// console.log(localStorage.getItem("theme"));
 	}
 
 	//To watch for changes:
@@ -15,11 +19,13 @@ window.onload = () => {
 };
 
 const btnToggleTheme = document.getElementById("themeToggle"),
-	spanMove = document.getElementById("pointThemeToggle");
+	spanMove = document.getElementById("pointThemeToggle"),
+	elementHTML = document.querySelector("html");
 
 btnToggleTheme.addEventListener("click", changeTheme);
 
 function changeTheme() {
+	elementHTML.classList.toggle();
 	spanMove.classList.toggle("move");
 	console.log(spanMove);
 }
