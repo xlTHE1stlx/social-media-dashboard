565b624c6ff0f3c973322027d553a7dc2f90e13d
Juan Olaya <jdos46juan@gmail.com>
2022-07-07 21:08:11 -0500

    modification focus to js, but add the class dark to body and add changes to html

--- js/index.js
+++ js/index.js
@@ -1,32 +1,58 @@
 window.onload = () => {
-	if (
-		window.matchMedia &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches &&
-		localStorage.getItem("theme") === "dark"
-	) {
-		// dark mode
-		localStorage.setItem("theme", "dark");
-		// console.log(localStorage.getItem("theme"));
+	if (!localStorage.getItem("theme")) {
+		if (
+			window.matchMedia &&
+			window.matchMedia("(prefers-color-scheme)").media !== "not all" &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches
+		) {
+			localStorage.setItem("theme", "dark");
+			document.querySelector("body").classList.add("dark");
+		} else {
+			localStorage.setItem("theme", "light");
+			document.querySelector("#pointThemeToggle").classList.add("move");
+		}
+	} else {
+		if (localStorage.getItem("theme") === "dark") {
+			console.log(localStorage.getItem("theme"));
+			document.querySelector("body").classList.add("dark");
+		} else {
+			document.querySelector("body").classList.remove("dark");
+			document.querySelector("#pointThemeToggle").classList.add("move");
+		}
 	}
-
-	//To watch for changes:
-
-	window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
-		const newColorScheme = e.matches ? "dark" : "light";
-		console.log(newColorScheme);
-	});
-	// localStorage.setItem()
 };
 
 const btnToggleTheme = document.getElementById("themeToggle"),
 	spanMove = document.getElementById("pointThemeToggle"),
-	elementHTML = document.querySelector("html");
+	body = document.querySelector("body");
 
 btnToggleTheme.addEventListener("click", changeTheme);
 
 function changeTheme() {
-	elementHTML.classList.toggle();
-	spanMove.classList.toggle("move");
-	console.log(spanMove);
+	// spanMove.classList.toggle("move");
+	if (localStorage.getItem("theme") === "dark") {
+		localStorage.setItem("theme", "light");
+		spanMove.classList.add("move");
+	} else {
+		localStorage.setItem("theme", "dark");
+		spanMove.classList.remove("move");
+	}
+
+	if (
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme)").media !== "not all" &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches &&
+		localStorage.getItem("theme") === "dark"
+	) {
+		body.classList.add("dark");
+	} else {
+		body.classList.remove("dark");
+	}
+
+	//To watch for changes:
+
+	// window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+	// 	const newColorScheme = e.matches ? "dark" : "light";
+	// 	console.log(newColorScheme);
+	// });
 }
-console.log(btnToggleTheme);
