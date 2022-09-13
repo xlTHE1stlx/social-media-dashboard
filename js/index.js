aae150decea3b3311517a1264e39ffb344af1cc9
Juan Olaya <jdos46juan@gmail.com>
2022-09-12 21:32:10 -0500

    complete creation of button for dark mode and light mode, futher, begin to created the media query for the website

--- js/index.js
+++ js/index.js
@@ -1,66 +1,29 @@
-window.onload = () => {
-	// if (!localStorage.getItem("theme")) {
-	// 	if (
-	// 		window.matchMedia &&
-	// 		window.matchMedia("(prefers-color-scheme)").media !== "not all" &&
-	// 		window.matchMedia("(prefers-color-scheme: dark)").matches
-	// 	) {
-	// 		localStorage.setItem("theme", "dark");
-	// 		document.querySelector("body").classList.add("dark");
-	// 	} else {
-	// 		localStorage.setItem("theme", "light");
-	// 		document.querySelector("#pointThemeToggle").classList.add("move");
-	// 	}
-	// } else {
-	// 	if (localStorage.getItem("theme") === "dark") {
-	// 		document.querySelector("body").classList.add("dark");
-	// 	} else {
-	// 		document.querySelector("body").classList.remove("dark");
-	// 		document.querySelector("#pointThemeToggle").classList.add("move");
-	// 	}
-	// }
-	// console.log("load");
-	// console.log(
-	// 	doc.addEventListener("permanentcolorscheme", (event) => {
-	// 		console.log(event.detail);
-	// 	})
-	// );
-	// console.log(
-	// 	doc.addEventListener("colorschemechange", (event) => {
-	// 		console.log(event);
-	// 	})
-	// );
-};
-
 const btnToggleTheme = document.getElementById("themeToggle"),
 	spanMove = document.getElementById("pointThemeToggle"),
 	body = document.querySelector("body");
 
+window.onload = () => {
+	spanMove.style.display = "none";
+	if (D_E.dataset.color === LIGHT) {
+		spanMove.classList.add("move");
+	}
+	spanMove.style.display = "flex";
+};
+
 btnToggleTheme.addEventListener("click", changeTheme);
 
 function changeTheme() {
-	const DARK = "dark",
-		LIGHT = "light";
-	if (localStorage.getItem(COLOR_THEME) === DARK) {
+	if (D_E.dataset.color === DARK) {
 		localStorage.setItem(COLOR_THEME, LIGHT);
 		spanMove.classList.add("move");
 		D_E.classList.remove(DARK);
 		D_E.classList.add(LIGHT);
 		D_E.dataset.color = LIGHT;
-	} else {
+	} else if (D_E.dataset.color === LIGHT) {
 		localStorage.setItem(COLOR_THEME, DARK);
 		spanMove.classList.remove("move");
 		D_E.classList.remove(LIGHT);
 		D_E.classList.add(DARK);
 		D_E.dataset.color = DARK;
 	}
-
-	// if (D_E.classList.includes(LIGHT))
-
-	// 	//To watch for changes:
-
-	// 	window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
-	// 		const newColorScheme = e.matches ? "dark" : "light";
-	// 		console.log(newColorScheme);
-	// 	});
 }
